2e1932b689e73b325c5f48ec399748215b70cc2d
alansam23 <alansamuel0023@gmail.com>
2023-06-07 11:06:58 +0530

    Added: Print ArrayElements

---
+++ ArrayElements.java
@@ -0,0 +1,8 @@
+public class ArrayElements {
+    public static void main(String[] args) {
+        int[] array = {1,2,5,8,10};
+        for (int i=0;i<array.length;i++){
+            System.out.print(array[i] +" ");
+        }
+    }
+}
